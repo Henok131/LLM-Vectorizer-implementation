87be05382dff5c751442815cb981999adaad3ff8
Henok Petros <137567720+Henok131@users.noreply.github.com>
2025-03-03 22:20:29 +0100

    deleted duplication

--- non-vectorized/incorrect_vectorization.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-float sum_array(float *array, int n) {
-    float sum = 0.0f;
-    for (int i = 0; i < n; i++) {
-        sum += array[i]; // Order-sensitive operation
-    }
-    return sum;
-}
